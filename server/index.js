f8300bf347065f7c091ae6415af65366dbefecd1
kian kheirani <kiankheirani@gmail.com>
2024-07-30 18:47:03 +0330

    '30'

--- server/index.js
+++ server/index.js
@@ -2,10 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-import helmet from "helmet";
-import morgan from "morgan";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+
+/*import { dirname } from "path";
+import { fileURLToPath } from "url";*/
 
 dotenv.config();
 import { connect } from "./connect/connect.js";
@@ -19,17 +18,17 @@ import management from "./routes/management.js";
 
 connect();
 
-app.use(cors());
+app.use(cors({ origin: process.env.FRONT }));
 app.use(express.json());
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.join(__dirname, "..", "/client/dist")));
+//const __dirname = dirname(fileURLToPath(import.meta.url));
+///app.use(express.static(path.join(__dirname, "..", "/client/dist")));
 app.use("/client", client);
 app.use("/management", management);
 app.use("/sale", sale);
 app.use("/general", generals);
-app.get("*", (req, res) => {
+/*app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "/client/dist/index.html"));
-});
+});*/
 app.listen(process.env.PORT, () => {
   console.log("app connected");
 });
